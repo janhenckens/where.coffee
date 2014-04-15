ad4ad9b9646c189c6e948fb9da5e1a83f3628716
Jan Henckens <jan@onedge.be>
2014-04-15 23:53:39 +0200

    Grunt JS tasks (jshint + concat) [touch: 5]

--- app/assets/Gruntfile.js
+++ app/assets/Gruntfile.js
@@ -2,6 +2,24 @@ module.exports = function(grunt) {
     // 1. All configuration goes here
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
+        jsconcat: {
+            main: {
+                src: [
+                    'js/libs/*.js', // All JS in the libs folder
+                    'js/components/*.js', // All JS in the components folder
+                    'js/main.js'  // The pbig main file!
+                ],
+                dest: 'build/js/main.js'
+            },
+        },
+        // JSHINT
+        jshint: {
+            options: {
+                reporter: require('jshint-stylish')
+            },
+            target: ['../../build/js/main.js', 'js/*.js']
+        },
         // SASS
         sass: {
             dist: {
@@ -15,36 +33,49 @@ module.exports = function(grunt) {
         },
         // CSSMIN
         cssmin: {
-             minify: {
-                 expand: true,
-                 cwd: '../../build/css/',
-                 src: ['*.css', '!*.min.css'],
-                 dest: '../../public/assets/css/',
-                 ext: '.min.css'
-             }
-         },
+            minify: {
+               expand: true,
+               cwd: '../../build/css/',
+               src: ['*.css', '!*.min.css'],
+               dest: '../../public/assets/css/',
+               ext: '.min.css'
+            }
+        },
         // NOTIFY
         notify: {
+            js: {
+                options: {
+                    message: 'JS task complete'
+                }
+            },
             sass: {
                 options: {
                     message: 'SASS compiled successfully'
                 }
-            }  
+            } 
         },
         // WATCH
-       watch: {
-           css: {
-               files: ['sass/**/*.scss'],
-               tasks: ['css', 'notify:css']
-           }
-       }
+        watch: {
+            options: {
+                livereload: true
+            },
+            sass: {
+                files: ['sass/**/*.scss'],
+                tasks: ['css', 'notify:sass']
+            },
+            js: {
+                files: ['js/**/*.js'],
+                tasks: ['js', 'notify:js'],
+                options: {
+                    spawn: false,
+                },
+            },
+        }
     });
 
-    // 2. Where we tell Grunt we plan to use this plug-in.
     require('load-grunt-tasks')(grunt);
-
-    // 3. Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('default', ['build' , 'watch']);
     grunt.registerTask('css', ['sass']);
-    grunt.registerTask('build', ['css' , 'cssmin']);
+    grunt.registerTask('js', ['jshint' , 'jsconcat']);
+    grunt.registerTask('build', ['css' , 'cssmin', 'jshint']);
 };
