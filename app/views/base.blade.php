424b43b915d9ea04f958b6467e7153563eb47f51
Jan Henckens <jan@onedge.be>
2014-04-18 21:37:01 +0200

    Base controller & template.

---
+++ app/views/base.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+	<title>Something</title>
+	<link rel="stylesheet" type="text/css" href="/assets/css/main.min.css">
+	<script type="text/javascript" src=""></script>
+</head>
+<body>
+	<div class="container">
+		@yield('content')
+	</div>
+</body>
+</html>
