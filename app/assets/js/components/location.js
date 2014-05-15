6564643c6ce8fc13387b017bb3dca29410a78292
Jan Henckens <jan@onedge.be>
2014-05-15 17:57:10 +0200

    Geolocation + ajax call.

--- app/assets/js/components/location.js
+++ app/assets/js/components/location.js
@@ -1,13 +1,39 @@
-// Get current location
-function showlocation() {
-   // One-shot position request.
-   navigator.geolocation.getCurrentPosition(callback);
-
-}
- 
-// Show current location
-function callback(position) {
-   document.getElementById('latitude').innerHTML = position.coords.latitude;
-   document.getElementById('longitude').innerHTML = position.coords.longitude;
-   document.getElementById('altitude').innerHTML = position.coords.altitude;
-}
+$(function(){
+  $('#getlocation').on('click', function(e){
+    console.log("Button Pressed (1)");
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+      error('not supported');
+    }
+    function success(position) {
+      console.log("Location found (2)");
+      var $long = position.coords.longitude;
+      var $lat = position.coords.latitude;
+      console.log("Longitude: " + $long);
+      console.log("Latitude: " + $lat);
+      // start ajax post request    
+      $.ajax({
+        url: 'location/get',
+        type: 'post',
+        data: {'latitude': $lat, 'longitude': $long},
+        success: function(data) {
+          var feed = jQuery.parseJSON(data);
+          console.log(feed.meta);
+          for (var i = 0, l=feed.response.groups[0].items.length ;i < l; i++) 
+          {
+            $( "#location" ).append(feed.response.groups[0].items[i].venue.name + "(" + feed.response.groups[0].items[i].venue.location.lat + "/" + feed.response.groups[0].items[i].venue.location.lng + ")<br>");
+          }
+        },
+        error: function(xhr, desc, err) {
+          console.log(xhr);
+          console.log("Details: " + desc + "\nError:" + err);
+        }
+      }); 
+      // end ajax call
+    }
+    function error(position) {
+      console.log(position);
+    }
+  });
+});
