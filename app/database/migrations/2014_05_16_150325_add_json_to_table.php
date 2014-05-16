3ee3b4aab51160c7ba9b1afa27d17835e40ae56b
Jan Henckens <jan@onedge.be>
2014-05-16 17:29:43 +0200

    Migrations.

---
+++ app/database/migrations/2014_05_16_150325_add_json_to_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddJsonToTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('locations', function(Blueprint $table)
+		{
+			$table->binary('json');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('locations', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
