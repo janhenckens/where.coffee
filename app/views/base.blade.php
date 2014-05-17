2538e3e13f0c2cc4236796fb880fcd5a109dfb79
Jan Henckens <jan@onedge.be>
2014-05-17 11:17:43 +0200

    mapbox js includes.

--- app/views/base.blade.php
+++ app/views/base.blade.php
@@ -1,10 +1,13 @@
 <!doctype html>
 <html>
 <head>
+	<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
 	<title>where.coffee</title>
 	<link rel="stylesheet" type="text/css" href="/assets/css/main.min.css">
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>        
 	<script type="text/javascript" src="/assets/js/main.min.js"></script>
+	<script src='https://api.tiles.mapbox.com/mapbox.js/v1.6.2/mapbox.js'></script>
+	<link href='https://api.tiles.mapbox.com/mapbox.js/v1.6.2/mapbox.css' rel='stylesheet' />
 </head>
 <body>
 @yield('content')
