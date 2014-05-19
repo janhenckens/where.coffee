ca8e993269cd99ee9d4582ca077af74e21a5864a
Jan Henckens <jan@onedge.be>
2014-05-19 20:57:06 +0200

    Fancy button stuff.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,5 +1,5 @@
 @section('content')
 	<div id='map'>
-	<button id="getlocation" class="btn location">Get location!</button>	
+	<button id="getlocation" class="btn btn-location">Get location!</button>	
 	</div>
 @stop
