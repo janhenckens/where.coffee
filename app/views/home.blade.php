c9ec1c6b7fbbad84f87ebc83a635e5883485e821
Jan Henckens <jan@onedge.be>
2014-05-20 22:49:09 +0200

    Newsletter link & small btn hover state.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,11 +3,11 @@
 	<div class="container">
 		<div class="panel">
 			<img class="icon" src="../assets/img/icon_white.png" width="100px">
-			<button id="getlocation" class="btn btn-location">Get location!</button>	
-			<input  type="text" id="searchlocation" class="btn btn-location search" placeholder="search for location">
-			<input class="btn btn-location searchsubmit" type="submit" value="go">
+			<button id="getlocation" class="btn btn-primary">Get location!</button>	
+			<input  type="text" id="searchlocation" class="btn btn-secondary search" placeholder="search for location">
+			<input class="btn btn-secondary searchsubmit" type="submit" value="go">
 			<button class="btn btn-small about">About</button>
-			<button class="btn btn-small newsletter">Newsletter</button>
+			<a href="http://newsletter.onedge.be/h/i/9B066A5DDB877BA1"><button class="btn btn-small newsletter">Newsletter</button></a>
 <!-- 			<img src="../assets/img/foursquare.png">
  -->		</div>
 
