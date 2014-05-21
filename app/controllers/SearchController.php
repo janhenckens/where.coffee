d3396dd85e2654192de7582641736e45cfb24854
Jan Henckens <jan@onedge.be>
2014-05-21 18:33:16 +0200

    variable encoding to cover spaces in location names.

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -24,7 +24,7 @@ class SearchController extends \BaseController {
 			/** Make file output JSON **/
 			header('Content-Type: application/json');
 			/** Receive long and lat through ajax **/
-			$query = Input::get('location');
+			$query = urlencode(Input::get('location'));
 			/** CURL to Foursquare to get the data based on the above mentioned $location **/
 			$url = "https://api.foursquare.com/v2/venues/explore?near=" . $query . "&client_id=R1RAIUEMOV013YRFZQWIPLV404ELYNWBM4ILDDYAIQUUYLLK&client_secret=KN5PYBT3QWCXK5SY3X0IHQZTWIMNBVIUV3RZ53NKFENUX41U&v=20130815&query=coffee";
 			$curl = curl_init();
