195cb0f42cca5e125fe785fdf6774d262a1b5f42
Jan Henckens <jan@onedge.be>
2014-06-17 19:37:17 +0200

    Limit results to 10.

--- app/controllers/LocationController.php
+++ app/controllers/LocationController.php
@@ -20,7 +20,7 @@ class LocationController extends BaseController {
 			$lat = Input::get('latitude');
 			$query = $lat . "," . $long;
 			/** CURL to Foursquare to get the data based on the above mentioned $query **/
-			$url = "https://api.foursquare.com/v2/venues/explore?client_id=R1RAIUEMOV013YRFZQWIPLV404ELYNWBM4ILDDYAIQUUYLLK&client_secret=KN5PYBT3QWCXK5SY3X0IHQZTWIMNBVIUV3RZ53NKFENUX41U&v=20130815&ll=" . $query . "&query=coffee";
+			$url = "https://api.foursquare.com/v2/venues/explore?client_id=R1RAIUEMOV013YRFZQWIPLV404ELYNWBM4ILDDYAIQUUYLLK&client_secret=KN5PYBT3QWCXK5SY3X0IHQZTWIMNBVIUV3RZ53NKFENUX41U&v=20130815&ll=" . $query . "&query=coffee&limit=10";
 			$curl = curl_init();
 			curl_setopt_array($curl, 
 				array(
