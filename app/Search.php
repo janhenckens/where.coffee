a613f908801b823e11445028c2d5c711764d9b33
Jan Henckens <jan@onedge.be>
2015-08-08 12:43:35 +0200

    Search model & assignments

---
+++ app/Search.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Search extends Model
+{
+    protected $fillable = [
+        'longitude',
+        'latitude',
+        'json',
+        'request_type'
+    ];
+
+}
