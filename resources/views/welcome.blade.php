a1cdc9c7117e22975e3366e69f816b7a2e5851bb
Jan Henckens <jan@onedge.be>
2015-08-25 20:04:12 +0200

    Different ids for the search forms

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,12 @@
         <div class="container">
             <div class="panel">
                 <img class="icon" src="../../img/icon.png" width="100px">
-                {!! Form::open(['id' => 'getlocation']) !!}
+                {!! Form::open(['id' => 'search-geo']) !!}
+                    {!! Form::hidden('request_type', 'geo') !!}
                     {!! Form::submit('Get my location', ['class' => 'btn btn-primary', 'id' => 'getlocation']) !!}
                 {!! Form::close() !!}
 
-                {!! Form::open(['id' => 'query']) !!}
+                {!! Form::open(['id' => 'search-location']) !!}
                 <div class="row">
                     <div class="col-md-8">
                         {!! Form::text('searchlocation', null, ['class' => 'form-control', 'placeholder' => 'search...']) !!}
